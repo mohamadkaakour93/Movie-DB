7073a4928d726d73d4c5c52ed84f2b772ff5a586
mohamadelkaakour <mhamadelkaakour@gmail.com>
2021-06-14 10:52:00 +0300

    step 5

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const d = new Date()
 const minutes = d.getMinutes() < 10 ? '0' + d.getMinutes() : '' + d.getMinutes();
 const hours = d.getHours()
 const TIME = hours + ":" + minutes
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
@@ -31,4 +38,19 @@ app.get('/hello/:id', function(req, res) {
 app.get('/search', function(req, res) {
     if(req.query.s == "" || req.query.s == undefined){res.status(500).send('You have to provide a search')}
     else{res.status(200).send("OK! You searched for:" + req.query.s)}
+}); 
+app.get('/movies/add', function(req, res) {
+    res.status(200).send("Create")
+});
+
+app.get('/movies/get', function(req, res) {
+    res.status(200).send(movies)
+});
+
+app.get('/movies/update', function(req, res) {
+    res.status(200).send("Update")
+});
+
+app.get('/movies/delete', function(req, res) {
+    res.status(200).send("Delete")
 }); 
