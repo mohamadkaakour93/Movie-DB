e3fcc48fb512d42aa89bb3bfc8f15f502db83b6e
mohamadelkaakour <mhamadelkaakour@gmail.com>
2021-06-14 10:29:55 +0300

    step 4

--- index.js
+++ index.js
@@ -20,3 +20,15 @@ app.get('/test', function(req, res) {
 app.get('/time', function(req, res) {
     res.status(200).send(TIME)
 });
+app.get('/hello', function(req, res) {
+    res.status(200).send("Hello")
+});
+
+app.get('/hello/:id', function(req, res) {
+    res.status(200).send("Hello " + req.params.id)
+});
+
+app.get('/search', function(req, res) {
+    if(req.query.s == "" || req.query.s == undefined){res.status(500).send('You have to provide a search')}
+    else{res.status(200).send("OK! You searched for:" + req.query.s)}
+}); 
